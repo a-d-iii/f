9401eedadfc8ab770281dc71e3f0806d5b28fa34
a-d-iii <adi.xv0@gmail.com>
2025-06-13 14:37:22 +0530

    chore: remove deprecated image plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image';
 import react from '@astrojs/react';
 
 export default defineConfig({
   output: 'static',
   integrations: [
     tailwind({ config: { applyBaseStyles: false } }),
-    image(),
     react(),
   ],
 });
