91fd87866529cb8772f43989284ab2de1ec1cfc8
a-d-iii <adi.xv0@gmail.com>
2025-06-13 14:59:26 +0530

    remove offline dataset

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,15 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://dwyojdeyfaozeeplpbyr.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3eW9qZGV5ZmFvemVlcGxwYnlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3OTY5MTMsImV4cCI6MjA2NTM3MjkxM30.A3EWWal-iREIyXX6j2F5Dzdi9KBTJQXAF1GHVcpDHY8';
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3eW9qZGV5ZmFvemVlcGxwYnlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3OTY5MTMsImV4cCI6MjA2NTM3MjkxM30.A3EWWal-iREIyXX6j2F5Dzdi9KBTJQXAF1GHVcpDHY8';
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export async function fetchLists() {
   try {
-    const { data, error } = await supabase
-      .from('lists')
-      .select('*');
+    const { data, error } = await supabase.from('lists').select('*');
     if (error) throw error;
     return data ?? [];
   } catch (err) {
